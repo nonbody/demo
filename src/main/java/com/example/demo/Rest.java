1ccc2d8c3909519f4c987aea1cd1e1ca55c74e89
non.t <nonbody.en.th@hotmail.com>
2017-09-25 22:35:16 +0700

    first commit hello

---
+++ src/main/java/com/example/demo/Rest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest {
+	
+	@GetMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "hello " + name;
+    }	
+
+}
