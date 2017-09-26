42216e41a9a3c0bb232b1006c18d53171ce0e7cb
non.t <nonbody.en.th@hotmail.com>
2017-09-26 23:33:01 +0700

    d

--- src/main/java/com/example/demo/Rest.java
+++ src/main/java/com/example/demo/Rest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RestX ZZZZZZZZZZZZZZZZZZZZ{
+public class RestFFFFFFFFFFFFZZZZZZZZZZZZZ{
 	
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
